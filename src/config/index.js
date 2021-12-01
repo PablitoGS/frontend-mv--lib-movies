0413a2cf31df9cb67d98688bda0eb64ccbf0660b
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-01 11:37:10 +0100

    feat(Root): config image width

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,5 @@ export const config = {
   API_KEY: '45f87653ea755690b034a9c960fac4b2',
   API_BASE_URL: 'https://api.themoviedb.org/3',
   IMAGES_BASE_URL: 'https://image.tmdb.org/t/p',
-  IMAGE_WIDTH: '/w500'
+  IMAGE_WIDTH: '/w300'
 }
