861bfa0424f8604fc22a9d0f6592b9ddb9446a4f
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-10 11:46:58 +0100

    feat(Root): entities

--- src/movies/Entities/MovieEntity.js
+++ src/movies/Entities/MovieEntity.js
@@ -1,3 +1,11 @@
 import {Entity} from '@s-ui/domain'
 
-export default class MovieEntity extends Entity {}
+export default class MovieEntity extends Entity {
+  /**
+   * @param {String} id
+   * @param {String} title
+   * @param {String} overview
+   * @param {String} posterPath
+   * @param {String} releaseDate
+   */
+}
